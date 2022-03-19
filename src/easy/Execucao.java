9e5c881f1e301708154e20fed1397f03c9c92cb7
Rudson Lee <rudsonlee@gmail.com>
2022-03-19 17:20:31 -0300

    exercicio02

---
+++ src/easy/Execucao.java
@@ -0,0 +1,8 @@
+package easy;
+
+public class Execucao {
+    public static void main(String[] args) {
+        Exercicio02 exercicio02 = new Exercicio02();
+        System.out.println(exercicio02.retornaPalavraDigitada("palavraDigitada"));
+    }
+}
