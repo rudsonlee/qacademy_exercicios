d4fcf2a7170f003f8f200a74e47ad6046ab4d846
Rudson Lee <rudsonlee@gmail.com>
2022-04-02 13:49:06 -0300

    exercicio01 Medium

--- src/medium/Execucao_medium.java
+++ src/medium/Execucao_medium.java
@@ -3,7 +3,7 @@ package medium;
 public class Execucao_medium {
     public static void main(String[] args) {
         Exercicio01 exercicio01 = new Exercicio01();
-        // exercicio01m.imparPar();
+        exercicio01.imparPar();
 
  
 
